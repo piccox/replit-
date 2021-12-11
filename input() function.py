3b6491e28b9339bcfb91cafa2cab5293c23b4409
PiccoxPy <>
2021-12-11 11:04:51 +0000

    Initial commit

---
+++ input() function.py
@@ -0,0 +1,10 @@
+input("what is your name?")
+
+print("Hello " + input("What is your name?"))
+
+# input() will get user input in console
+
+
+
+# input() Coding Exercise 
+print(len(input("what is your name?")))
---
+++ variable.py
@@ -0,0 +1,58 @@
+name = input("where do you come from?")
+
+
+name = "Jack"
+print(name)
+name = "Angela"
+print(name)
+
+
+
+name = input("what is your name?")
+length = len(name)
+print(length)
+
+
+#coding exercise
+a = input("a:")
+b = input("b:")
+
+
+
+
+
+## rules are around to be variables 
+c = a
+a = b
+b = c
+#applying a logic 
+a = input("a:")
+b = input("b:")
+
+
+
+
+### variable names 
+name = 'Jack'
+user_name = "what is the day today?"
+
+
+
+
+#### Band names generators
+#1. create a greeting for your program. 
+print("Welcome to the band name generator!")
+
+#2. Ask the user for the city that they grew up in. 
+city = input("Which city did you grow up in ? \n")
+
+#3. Aske the user for the name of a pet. 
+pet = input("What is the name of a pet\n")
+
+#4. Combine the name of their city and pet show them their band name. 
+city_pet = print("Your band name could be" + city + "" + pet)
+
+#5. Make sure the input cursor shows on a new line, see the example at: http://band-name-generator-end.apprbrewery.repl.run/
+
+
+
