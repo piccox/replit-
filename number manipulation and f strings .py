97ed909cd232a0944912f8d8280da36f99aec1dd
Meeyo <95964965+piccox@users.noreply.github.com>
2021-12-12 11:09:05 +0000

    and f-string

---
+++ number manipulation and f strings .py
@@ -0,0 +1,18 @@
+# User scores a point 
+# as it is; a = a + 1 
+
+score = 0
+#score += 1 
+#print(score)
+height = 1.8
+iswinning = True
+
+
+
+# f - strings; 
+print(f"your score is {score}, your height is {height}, you are winning is {iswinning}")
+
+
+
+
+
