bb699dc0e3110c2178414c5e8002653c6f2d3671
Meeyo <95964965+piccox@users.noreply.github.com>
2021-12-13 06:08:36 +0000

    coding excercise

---
+++ even and odd.py
@@ -0,0 +1,28 @@
+# Odd numbers:  remainder 1
+a = 7 % 2
+print(a)
+
+ # Even numbers: not remainder 
+ #when give a result 0 is even 
+b = 2 % 2
+print(b)
+
+
+
+
+#coding exercise; 
+number = 94
+
+if number % 2 == 0: 
+  print("this is an even number! ")
+
+else: 
+  print("this is an odd number.")
+
+
+
+
+
+
+
+
