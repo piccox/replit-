45b618143d5994c0a5ebd9e48f110fb3e700b719
Meeyo <95964965+piccox@users.noreply.github.com>
2021-12-13 05:43:32 +0000

    control flow  and conditional operators

--- if and else.py
+++ if and else.py
@@ -1,7 +1,27 @@
+height = int(input("what is your height in cm?"))
+
+#if True or False
+# > ; greater than 
+if height > 120: 
+  print("you can ride the rollercoaster!")
+else: 
+  print("sorry, you can not ride!")
 
 
 
 
+# == precisely equal to 
+if height == 120: 
+  print("you can ride the rollercoaster!")
+else: 
+  print("sorry, you can not ride!")
+
+
+# height >= ; greater or equal to 
+if height >= 120: 
+  print("you can ride the rollercoaster!")
+else: 
+  print("sorry, you can not ride!")
 
 
 
