dff3c2a2f9e8a274b127eec03da272f65ee68c08
Meeyo <95964965+piccox@users.noreply.github.com>
2022-01-02 11:03:35 +0000

    highest score

--- Section 5: Day 5/high score.py
+++ Section 5: Day 5/high score.py
@@ -1,42 +1,25 @@
 #how to find the highest score from a List of scores. 
-# [78, 65, 89, 86, 55, 91, 64, 89]
-
-
+# 78, 65, 89, 86, 55, 91, 64, 89
 student_scores = input("input a list of student score \n").split(',')
-print(student_scores)
-
+#print(student_scores)
 
 for n in range(0, len(student_scores)):
-  print(n, student_scores[n])
+  #print(n, student_scores[n])
   student_scores[n] = int(student_scores[n])
-
 print(student_scores)
-#print(min(student_scores))
-#print(max(student_scores))
 
-
-
-print("Start loop")
-
+print("------\n")
 highest_score = 0
-second_highest_score = 0 
-#h = []
-for scores in student_scores:
-  
-  if scores > highest_score: 
-    #h.append(scores)
-    highest_score = scores
-
-    print("now high score is ", highest_score)
-  
-  elif second_highest_score < scores :
-    second_highest_score = scores
-
-
-  else:
+for score in student_scores: 
+  if score > highest_score:
+    highest_score = score
+    print("new high score", highest_score)
+  else: 
     print(False)
+print(f"highest_score is {score}, {highest_score}")
 
-print(f"The highest score in the class is : {highest_score}")
+
+
 
 
 
