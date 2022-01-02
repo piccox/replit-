2e5a071e5120c97bbf50a4eeba53c3d4dd771924
Meeyo <95964965+piccox@users.noreply.github.com>
2022-01-02 11:20:56 +0000

    highest score and second highest score

--- Section 5: Day 5/secondhighestscore.py
+++ Section 5: Day 5/secondhighestscore.py
@@ -1,21 +1,21 @@
 #find Second higest score in the list 
 student_score = [98,79,98,97,96,98,90,91,80,83]
+
 highest_score = 0 
 second_high = 0
-
 for i in student_score:
-  print('i ---->', i)
+  print('--------',i,'---------')
   if i > highest_score: 
     second_high = highest_score
     highest_score = i 
 
-    print(f"i is {i}\n,high score is {highest_score} \n, second is      {second_high}")
+    print(f"[if] i is {i},high score is {highest_score}, second is      {second_high}")
    
 
   elif second_high < i and highest_score> i:
     second_high = i 
-    print('second high score -->', second_high)
+    print('[elif] second high score -->', second_high)
     
   else: 
-    pass 
+    print('[else] nothing happened for', i) 
 print(highest_score,second_high)
