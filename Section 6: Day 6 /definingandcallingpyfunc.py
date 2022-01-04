22b679e887d667d3a8f223f6fc69699b2e419ccd
Meeyo <95964965+piccox@users.noreply.github.com>
2022-01-04 07:20:40 +0000

    Karel the robot

---
+++ Section 6: Day 6 /definingandcallingpyfunc.py
@@ -0,0 +1,57 @@
+# how make our own function 
+def my_func():
+  print("hello")
+  print("bye")
+
+  #calling the function 
+my_func()
+
+
+# How to create function 
+# do this /then do this/finally do this 
+# Calling functions 
+
+###reeborg.ca: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
+
+
+### creat a function how to turn_left() 
+#def turn_around():
+  #turn_left()
+  #turn_left()
+   
+#move()
+#move()
+#turn_around()
+#move()
+#move()
+#turn_around()
+
+
+
+
+### create a function, how to turn_right()
+#def turn_around():
+  #turn_left()
+  #turn_left()
+ 
+#def turn_right():
+  #turn_left()
+  #turn_left()
+  #turn_left()
+
+#draw square 
+#turn_left()
+#move()
+#turn_right()
+#move()
+#turn_right()
+#move()
+#turn_right()
+#move()
+
+
+
+
+
+
+
