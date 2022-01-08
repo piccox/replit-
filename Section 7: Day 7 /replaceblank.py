ab21cfa8803595a66dda12cfecec42130892f22e
Meeyo <95964965+piccox@users.noreply.github.com>
2022-01-08 10:45:16 +0000

    replacing the blanks

---
+++ Section 7: Day 7 /replaceblank.py
@@ -0,0 +1,27 @@
+#replacing Blanks with Guesses 
+
+word_list = ["ardvark", "baboon", "camel"]
+ 
+import random 
+chosen_word = random.choice(word_list)
+#testing code
+print(f'Pssst,the solution is {chosen_word}')
+# To do 1, create an empty list called a display
+#e.g. letter in the chosen_word, add a "_" to 'display'
+#so if the chosen_word was "apple", display should be ["_","_","_","_","-"] with 5 "_" representing each letter to guess. 
+display = []
+for _ in range(len(chosen_word)): 
+  display += "_"
+print(display)
+
+guess = input("Guess a letter: ").lower()
+
+#Todo 2, loop through each position in the chosen_word:
+for position in range(len(chosen_word)):
+  letter = chosen_word[position]
+  if letter == guess: 
+    display[position] = letter 
+  print(display)
+
+ 
+    
