4c45f7e83c6cdeb25f993e1a14562973268a6fdb
Meeyo <95964965+piccox@users.noreply.github.com>
2022-01-27 07:03:10 +0000

    Nesting Dictionaries

---
+++ Section 9/nestinglistanddict.py
@@ -0,0 +1,7 @@
+#Nesting Lists and Dictionary
+travel_log = {"France":{"cities_visited":["Paris","Lille","Leon"],"total_visits":12}, "Korea":{"cities_visited":["Seoul","Busan","Jejudo"],"total_visits":3}}
+print(travel_log)
+
+#Nesting Dictoionary in a list
+travel_log = [{"country":"France","cities_visited":["Paris","Lille","Leon"],"total_visits":12},
+ {"country":"Korea","cities_visited":["Seoul","Busan","Jejudo"],"total_visits":3}]
