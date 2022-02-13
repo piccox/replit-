24b88cdb39aa3365c344806d4e0b95c13ce987e0
Meeyo <95964965+piccox@users.noreply.github.com>
2022-02-13 05:50:11 +0000

    functions with outputs

---
+++ Section 10  -  14 : /functions with outputs.py
@@ -0,0 +1,42 @@
+# Functions with Outputs
+
+# Type of a function 
+'''
+Functions 
+
+  do this 
+  then do this
+  finally do this
+
+'''
+
+# type of an other function 
+
+'''
+def my_function(something):
+  do this with something
+  then do this
+  finally do this 
+
+  finally calling the function 
+
+
+'''
+
+
+
+
+def format_name(first_name, last_name): 
+  # two inputs 
+  # convert to string to title() case in python 
+
+  formated_first_name = first_name.title()
+  formated_last_name = last_name.title()
+  return f"{formated_first_name} {formated_last_name}" 
+  
+
+print(format_name("amy","WineHouse"))
+
+
+
+ 
